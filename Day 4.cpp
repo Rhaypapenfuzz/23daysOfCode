8d890f957a347a2da3c939efce3b71f9f61f2f7c
Raymond Papenfuss Gyabeng <39167251+Rhaypapenfuzz@users.noreply.github.com>
2019-01-06 00:37:31 -0500

    Update Day 4.cpp
    Modify the previous program(the program for Day 3) such that only multiples of three or five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17

--- Day 4.cpp
+++ Day 4.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+int accumulate(int);
 
+int main() {
+
+  int userInput; 
+  //Ask user for int number
+  cout<<"Enter any number you want to calculate the sum to: ";
+  cin>>userInput; //store input
+  cout<<accumulate(userInput); 
+  return 0;
+}
+
+//function to calculate sum from 1 to userInput
+int accumulate(int userInput){
+  int sum = 0;
+  for(int i = 1;i < userInput + 1 ; i++){
+    //check if number is multiple of 3 or 5
+    if( i%3 == 0 || i%5 == 0) {
+    sum += i;
+    } 
+  }
+  return sum;
+}
