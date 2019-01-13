4e825b9852b946a71840b2450d29d3a189fc4b1d
Raymond Papenfuss Gyabeng <39167251+Rhaypapenfuzz@users.noreply.github.com>
2019-01-13 00:28:23 -0500

    Update Day_11.py
    Given a non-negative integer numRows, generate the first numRows of Pascal's triangle. In Pascal's triangle, each number is the sum of the two numbers directly above it.
    Example: Input: 5
    Output:[ [1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1] ]

--- Day_11.py
+++ Day_11.py
@@ -4,26 +4,25 @@ class Solution:
         :type numRows: int
         :rtype: List[List[int]]
         """
-        myList = [1]
-        Pascals_triangle = []
-        if numRows <= 0:
-            return Pascals_triangle
-            
-        first = 0
-        second = 1
-        Pascals_triangle.append([elem for elem in myList])      #[ [1] ]
-        
-        for i in range(0, numRows - 1):
-            next_number = first + second
-            myList.insert(int(((len(myList))/2)), next_number)
-            if len(myList) % 2 == 0:
-              myList[int((len(myList)/2))] = next_number
-            Pascals_triangle.append([elem for elem in myList])
-            first = second
-            second = next_number
-            if len(myList) % 2 == 0:
-              first = next_number
-        print(Pascals_triangle)  
+        myList = []      # Inner List
+        Pascals_triangle = []   # Overall List
+
+        first_counter_1 = 0     
+        for i in range(1, numRows + 1):
+          first_counter_1 += 1 
+          current_number = 1   # Set first number back to 1
+
+          second_counter = 0
+          for j in range(1, first_counter_1 + 1):
+            second_counter += 1
+
+            myList.insert(second_counter, int(current_number))
+            current_number = current_number * (first_counter_1 - second_counter)/second_counter # Pascal triangle calculation
+          Pascals_triangle.append([elem for elem in myList])  # Put myList in larger List
+          myList.clear()  # Empty current myList
+        print(Pascals_triangle)
+        return(Pascals_triangle) 
 
 trial = Solution()
-trial.generate(4)
+trial.generate(int(input("Enter the number of lines of Pascal's triangle you want to generate: "))) #Call function from class with user input as argument
+
